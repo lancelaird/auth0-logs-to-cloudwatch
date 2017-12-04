c1fc470733dc267f9b06bc7b1c9959eb736596e5
Matthew Ceroni <matthew.ceroni@dat.com>
2017-12-04 11:18:36 -0800

    Added AWS_REGION as a required setting.

--- build/bundle.js
+++ build/bundle.js
@@ -68,7 +68,7 @@ module.exports =
 
 	function lastLogCheckpoint(req, res) {
 	  var ctx = req.webtaskContext;
-	  var required_settings = ['AUTH0_DOMAIN', 'AUTH0_CLIENT_ID', 'AUTH0_CLIENT_SECRET', 'AWS_ACCESS_KEY', 'AWS_SECRET_KEY'];
+	  var required_settings = ['AUTH0_DOMAIN', 'AUTH0_CLIENT_ID', 'AUTH0_CLIENT_SECRET', 'AWS_ACCESS_KEY', 'AWS_SECRET_KEY', 'AWS_REGION'];
 	  var missing_settings = required_settings.filter(function (setting) {
 	    return !ctx.data[setting];
 	  });
@@ -76,6 +76,7 @@ module.exports =
 	  if (!lawger) {
 	    var aws_access_key = ctx.data.AWS_ACCESS_KEY;
 	    var aws_secret_key = ctx.data.AWS_SECRET_KEY;
+      var aws_region = ctx.data.AWS_REGION;
 	    var log_group_name = ctx.data.LOG_GROUP || 'AUTH0_GROUP';
 	    var log_stream_name = ctx.data.LOG_STREAM || ctx.data.AUTH0_DOMAIN;
 
@@ -83,7 +84,7 @@ module.exports =
 	      aws: {
 	        accessKeyId: aws_access_key,
 	        secretAccessKey: aws_secret_key,
-	        region: 'us-east-1'
+	        region: aws_region
 	      }
 	    });
 	    lawger = lawgs.getOrCreate(log_group_name);
@@ -521,7 +522,7 @@ module.exports =
 /* 9 */
 /***/ (function(module, exports) {
 
-	module.exports = {"title":"Auth0 Logs to AWS CloudWatch","name":"auth0-logs-to-cloudwatch","version":"1.0.1","author":"saltuk, bbeck","description":"This extension will take all of your Auth0 logs and export them to AWS CloudWatch","type":"cron","repository":"https://github.com/TDG-Platform/auth0-logs-to-cloudwatch","keywords":["auth0","extension"],"schedule":"0 */5 * * * *","auth0":{"scopes":"read:logs"},"secrets":{"BATCH_SIZE":{"description":"The ammount of logs to be read on each execution. Maximun is 100.","default":100},"LOG_LEVEL":{"description":"This allows you to specify the log level of events that need to be sent","type":"select","allowMultiple":true,"options":[{"value":"-","text":""},{"value":"0","text":"Debug"},{"value":"1","text":"Info"},{"value":"2","text":"Warning"},{"value":"3","text":"Error"},{"value":"4","text":"Critical"}]},"LOG_TYPES":{"description":"If you only want to send events with a specific type (eg: failed logins)","type":"select","allowMultiple":true,"options":[{"value":"-","text":""},{"value":"s","text":"Success Login (Info)"},{"value":"seacft","text":"Success Exchange (Info)"},{"value":"feacft","text":"Failed Exchange (Error)"},{"value":"f","text":"Failed Login (Error)"},{"value":"w","text":"Warnings During Login (Warning)"},{"value":"du","text":"Deleted User (Info)"},{"value":"fu","text":"Failed Login (invalid email/username) (Error)"},{"value":"fp","text":"Failed Login (wrong password) (Error)"},{"value":"fc","text":"Failed by Connector (Error)"},{"value":"fco","text":"Failed by CORS (Error)"},{"value":"con","text":"Connector Online (Info)"},{"value":"coff","text":"Connector Offline (Error)"},{"value":"fcpro","text":"Failed Connector Provisioning (Critical)"},{"value":"ss","text":"Success Signup (Info)"},{"value":"fs","text":"Failed Signup (Error)"},{"value":"cs","text":"Code Sent (Debug)"},{"value":"cls","text":"Code/Link Sent (Debug)"},{"value":"sv","text":"Success Verification Email (Debug)"},{"value":"fv","text":"Failed Verification Email (Debug)"},{"value":"scp","text":"Success Change Password (Info)"},{"value":"fcp","text":"Failed Change Password (Error)"},{"value":"sce","text":"Success Change Email (Info)"},{"value":"fce","text":"Failed Change Email (Error)"},{"value":"scu","text":"Success Change Username (Info)"},{"value":"fcu","text":"Failed Change Username (Error)"},{"value":"scpn","text":"Success Change Phone Number (Info)"},{"value":"fcpn","text":"Failed Change Phone Number (Error)"},{"value":"svr","text":"Success Verification Email Request (Debug)"},{"value":"fvr","text":"Failed Verification Email Request (Error)"},{"value":"scpr","text":"Success Change Password Request (Debug)"},{"value":"fcpr","text":"Failed Change Password Request (Error)"},{"value":"fn","text":"Failed Sending Notification (Error)"},{"value":"limit_wc","text":"Blocked Account (Critical)"},{"value":"limit_ui","text":"Too Many Calls to /userinfo (Critical)"},{"value":"api_limit","text":"Rate Limit On API (Critical)"},{"value":"sdu","text":"Successful User Deletion (Info)"},{"value":"fdu","text":"Failed User Deletion (Error)"}]},"START_FROM":{"description":"The Auth0 LogId from where you want to start."},"AWS_ACCESS_KEY":{"description":"AWS access key"},"AWS_SECRET_KEY":{"description":"AWS secret key"},"LOG_GROUP":{"description":"Log group"},"LOG_STREAM":{"description":"Log stream"}}}
+	module.exports = {"title":"Auth0 Logs to AWS CloudWatch","name":"auth0-logs-to-cloudwatch","version":"1.0.1","author":"saltuk, bbeck","description":"This extension will take all of your Auth0 logs and export them to AWS CloudWatch","type":"cron","repository":"https://github.com/TDG-Platform/auth0-logs-to-cloudwatch","keywords":["auth0","extension"],"schedule":"0 */5 * * * *","auth0":{"scopes":"read:logs"},"secrets":{"BATCH_SIZE":{"description":"The ammount of logs to be read on each execution. Maximun is 100.","default":100},"LOG_LEVEL":{"description":"This allows you to specify the log level of events that need to be sent","type":"select","allowMultiple":true,"options":[{"value":"-","text":""},{"value":"0","text":"Debug"},{"value":"1","text":"Info"},{"value":"2","text":"Warning"},{"value":"3","text":"Error"},{"value":"4","text":"Critical"}]},"LOG_TYPES":{"description":"If you only want to send events with a specific type (eg: failed logins)","type":"select","allowMultiple":true,"options":[{"value":"-","text":""},{"value":"s","text":"Success Login (Info)"},{"value":"seacft","text":"Success Exchange (Info)"},{"value":"feacft","text":"Failed Exchange (Error)"},{"value":"f","text":"Failed Login (Error)"},{"value":"w","text":"Warnings During Login (Warning)"},{"value":"du","text":"Deleted User (Info)"},{"value":"fu","text":"Failed Login (invalid email/username) (Error)"},{"value":"fp","text":"Failed Login (wrong password) (Error)"},{"value":"fc","text":"Failed by Connector (Error)"},{"value":"fco","text":"Failed by CORS (Error)"},{"value":"con","text":"Connector Online (Info)"},{"value":"coff","text":"Connector Offline (Error)"},{"value":"fcpro","text":"Failed Connector Provisioning (Critical)"},{"value":"ss","text":"Success Signup (Info)"},{"value":"fs","text":"Failed Signup (Error)"},{"value":"cs","text":"Code Sent (Debug)"},{"value":"cls","text":"Code/Link Sent (Debug)"},{"value":"sv","text":"Success Verification Email (Debug)"},{"value":"fv","text":"Failed Verification Email (Debug)"},{"value":"scp","text":"Success Change Password (Info)"},{"value":"fcp","text":"Failed Change Password (Error)"},{"value":"sce","text":"Success Change Email (Info)"},{"value":"fce","text":"Failed Change Email (Error)"},{"value":"scu","text":"Success Change Username (Info)"},{"value":"fcu","text":"Failed Change Username (Error)"},{"value":"scpn","text":"Success Change Phone Number (Info)"},{"value":"fcpn","text":"Failed Change Phone Number (Error)"},{"value":"svr","text":"Success Verification Email Request (Debug)"},{"value":"fvr","text":"Failed Verification Email Request (Error)"},{"value":"scpr","text":"Success Change Password Request (Debug)"},{"value":"fcpr","text":"Failed Change Password Request (Error)"},{"value":"fn","text":"Failed Sending Notification (Error)"},{"value":"limit_wc","text":"Blocked Account (Critical)"},{"value":"limit_ui","text":"Too Many Calls to /userinfo (Critical)"},{"value":"api_limit","text":"Rate Limit On API (Critical)"},{"value":"sdu","text":"Successful User Deletion (Info)"},{"value":"fdu","text":"Failed User Deletion (Error)"}]},"START_FROM":{"description":"The Auth0 LogId from where you want to start."},"AWS_ACCESS_KEY":{"description":"AWS access key"},"AWS_SECRET_KEY":{"description":"AWS secret key"},"AWS_REGION":{"description":"AWS region"},"LOG_GROUP":{"description":"Log group"},"LOG_STREAM":{"description":"Log stream"}}}
 
 /***/ }),
 /* 10 */
@@ -578,7 +579,7 @@ module.exports =
 
 		var uploadQueuedLogs = function(logs) {
 			_log(logGroupName, '>> uploadQueuedLogs triggered with ', logs.length, ' logs');
-			
+
 			var createLogGroup = Q(true);
 			if(!logGroupExists) {
 				createLogGroup = _createLogGroupIfDoesntExist(logGroupName)
@@ -640,7 +641,7 @@ module.exports =
 		// Public API
 		this.config = function(conf) {
 			extend(this, conf);
-			
+
 			if(conf.settings) {
 				settings = conf.settings;
 			}
@@ -651,10 +652,10 @@ module.exports =
 		};
 
 		this.log = function(type, obj) {
-			obj = { 
-				message: typeof obj === 'string' ? obj : JSON.stringify(obj), 
-				timestamp: new Date().getTime(), 
-				type: type 
+			obj = {
+				message: typeof obj === 'string' ? obj : JSON.stringify(obj),
+				timestamp: new Date().getTime(),
+				type: type
 			};
 
 			logsSource.onNext(obj);
@@ -681,7 +682,7 @@ module.exports =
 				else		deferred.resolve(data.logGroups.length > 0);
 			});
 
-			return deferred.promise.timeout(settings.aws.timeout 
+			return deferred.promise.timeout(settings.aws.timeout
 				|| DEFAULT_TIMEOUT, 'Could not communicate with AWS CloudWatch in a timely fashion');
 		}
 
@@ -694,7 +695,7 @@ module.exports =
 				else		deferred.resolve(name);
 			});
 
-			return deferred.promise.timeout(settings.aws.timeout 
+			return deferred.promise.timeout(settings.aws.timeout
 				|| DEFAULT_TIMEOUT, 'Could not create log group in a timely fashion');
 		}
 
@@ -720,7 +721,7 @@ module.exports =
 				else		deferred.resolve(data.logStreams.length > 0);
 			});
 
-			return deferred.promise.timeout(settings.aws.timeout 
+			return deferred.promise.timeout(settings.aws.timeout
 				|| DEFAULT_TIMEOUT, 'Could not communicate with AWS in a timely fashion');
 		}
 
@@ -733,7 +734,7 @@ module.exports =
 				else		deferred.resolve(name);
 			});
 
-			return deferred.promise.timeout(settings.aws.timeout 
+			return deferred.promise.timeout(settings.aws.timeout
 				|| DEFAULT_TIMEOUT, 'Could not create log stream in a timely fashion');
 		}
 
@@ -766,7 +767,7 @@ module.exports =
 				else deferred.resolve(data);
 			});
 
-			return deferred.promise.timeout(settings.aws.timeout 
+			return deferred.promise.timeout(settings.aws.timeout
 				|| DEFAULT_TIMEOUT, 'Could not communicate with AWS in a timely fashion');
 		}
 
